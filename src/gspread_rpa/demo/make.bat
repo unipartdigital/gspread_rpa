d5cd6feda9ec3daf2df3d44f9244196e67d0f1f9
Ali Bendriss <ali.bendriss@unipart.io>
2022-02-20 02:12:01 +0000

    poor man windows makefile
     by entering the directory
     'make' will run the demo
     'make clean' will clean up the directory from logs and stamp files
    
    	new file:   demo/make.bat

---
+++ src/gspread_rpa/demo/make.bat
@@ -0,0 +1,34 @@
+:parse
+if "%~1"=="" goto main
+if "%~1"=="clean" goto clean
+shift
+goto parse
+:endparse
+REM endparse
+
+:main
+echo off
+for /f %%a in ('dir /b "*.py"') do (
+    if exist %%a.done (
+     for /f "skip=2 tokens=*" %%b in ('dir /b /TW /OD "%%a*"') do if "%%a" == "%%b" (
+         (call python %%a && echo > "%%a.done" ) || goto exiterror
+     ) else echo make: Nothing to be done for %%a
+    ) else (call python %%a && echo > "%%a.done" ) || goto exiterror
+)
+goto end
+
+:clean
+echo off
+
+(for /f %%a in ('dir /b "*.py.log"'  ) do echo "DELETE %%a" && del "%%a" ) 2> nul
+(for /f %%a in ('dir /b "*.py.log.*"') do echo "DELETE %%a" && del "%%a" ) 2> nul
+(for /f %%a in ('dir /b "*.py.done"' ) do echo "DELETE %%a" && del "%%a" ) 2> nul
+(for /f %%a in ('dir /b "*~"'        ) do echo "DELETE %%a" && del "%%a" ) 2> nul
+goto end
+
+:exiterror
+echo on
+exit /b 1
+
+:end
+echo on
